80645c826458b8e1432b23dc2c001c364bc133b4
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-06-09 13:37:09 +0530

    Updated: Index and app files

--- client/context/socket.context.tsx
+++ client/context/socket.context.tsx
@@ -2,12 +2,17 @@ import { io } from "../node_modules/socket.io-client/build/esm/index";
 import { SOCKET_URL } from "../conifg/default";
 import { createContext, useContext } from "react";
 
-const SocketContext = createContext({});
+const socket = io(SOCKET_URL);
 
-const SocketsProvideer = (props: any) => {
-  return <SocketContext.Provider value={{}}>{...props}</SocketContext.Provider>;
+const SocketContext = createContext({ socket });
+
+const SocketsProvider = (props: any) => {
+  return (
+    <SocketContext.Provider value={{ socket }} {...props} />
+      
+  );
 };
 
-export const useSockets=()=>useContext(SocketContext)
+export const useSockets = () => useContext(SocketContext);
 
-export default SocketsProvideer; 
+export default SocketsProvider;
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,16 @@
-import Head from '../node_modules/next/head'
-import Image from '../node_modules/next/image'
-import styles from '../styles/Home.module.css'
+import Head from "../node_modules/next/head";
+import Image from "../node_modules/next/image";
+import styles from "../styles/Home.module.css";
+import { useSockets } from "../context/socket.context";
 
 export default function Home() {
+  const {socket}=useSockets()
+
   return (
     <>
-    
+      <div>
+        <h1>Welcome to the chat! {socket.id}</h1>
+      </div>
     </>
-  )
+  );
 }
